6b06e287e6092f86b830aa8d9e426b87b346794b
Giorgio Peron <giorgioperon@luxottica.com>
2009-10-25 18:59:44 +0100

    cherry-pick dev-1

--- jpo/src/main/java/org/gperon/jpo/ClassJpo.java
+++ jpo/src/main/java/org/gperon/jpo/ClassJpo.java
@@ -11,4 +11,25 @@ package org.gperon.jpo;
  */
 public class ClassJpo {
 
+    protected String name = "Cammello";
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
