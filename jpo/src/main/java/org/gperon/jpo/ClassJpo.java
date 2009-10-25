cab7e7d0b15bb0763b20d5ae3b784a1b2f763ef3
Giorgio Peron <giorgioperon@luxottica.com>
2009-10-25 18:49:46 +0100

    add property name

--- jpo/src/main/java/org/gperon/jpo/ClassJpo.java
+++ jpo/src/main/java/org/gperon/jpo/ClassJpo.java
@@ -10,8 +10,30 @@ package org.gperon.jpo;
  * @author GiorgioP
  */
 public class ClassJpo {
+
+    protected String name = "Cammello";
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello Giorgio");
     }
+
 
 }
