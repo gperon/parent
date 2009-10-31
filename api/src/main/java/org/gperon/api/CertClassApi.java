10351ec6ee31ac59e35cbfa9b60a33d807edeeb1
Giorgio Peron <giorgioperon@luxottica.com>
2009-10-31 15:45:56 +0100

    X

---
+++ api/src/main/java/org/gperon/api/CertClassApi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gperon.api;
+
+/**
+ *
+ * @author GiorgioP
+ */
+public class CertClassApi {
+
+    private void x(String msg) {
+        System.out.println("x");
+    }
+}
