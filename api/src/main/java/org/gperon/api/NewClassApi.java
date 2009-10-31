8cf75acdc480e075a2596829d62bd2cb699b4890
Giorgio Peron <giorgioperon@luxottica.com>
2009-10-31 15:41:27 +0100

    A

---
+++ api/src/main/java/org/gperon/api/NewClassApi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gperon.api;
+
+/**
+ *
+ * @author GiorgioP
+ */
+public class NewClassApi {
+
+    private void a(String msg) {
+        System.out.println("a");
+    }
+}
