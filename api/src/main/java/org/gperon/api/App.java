ace07d245fae934a7e403e0e75c31427b8693c89
Giorgio Peron <giorgioperon@luxottica.com>
2009-10-31 15:21:43 +0100

    W

--- api/src/main/java/org/gperon/api/App.java
+++ api/src/main/java/org/gperon/api/App.java
@@ -4,10 +4,13 @@ package org.gperon.api;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void w(String[] args) {
+        System.out.println("w");
     }
 }
